1d2c60451567c2acab25f4b226f22509c94084b4
thomasliu <thomas_0836@qq.com>
2020-10-09 23:12:11 +0800

    基础环境

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,37 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+  <view class="content">
+    <image class="logo" src="../../static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-	export default Vue.extend({
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	});
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Hello1',
+    }
+  },
+  onLoad() {},
+  methods: {},
+})
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+.content {
+  text-align: center;
+  height: 400upx;
+}
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
+.title {
+  font-size: 36upx;
+  color: #8f8f94;
+}
 </style>
---
+++ stylelint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss'],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['screen'],
+      },
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
+    'unit-no-unknown': [
+      true,
+      {
+        ignoreUnits: ['upx'],
+      },
+    ],
+  },
+}
